72b1aeac273d77630f8e61df8fda7ec709ee6918
hspyo <olok95@naver.com>
2020-07-11 14:35:43 +0900

    JS: Weather API

--- VanillaJS/weather.js
+++ VanillaJS/weather.js
@@ -1,10 +1,26 @@
+const weather = document.querySelector(".js-weather");
 const API_KEY ="5253262c3b88bb92e872591ba3ce70bc";
 const COORDS = "coords";
 
+// 실시간으로 날씨데이터를 받아옴
+function getWeather(lat, lng){
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+  ).then(function(response){
+    return response.json();
+  }).then(function(json){
+    const temperature = json.main.temp;
+    const place = json.name;
+    weather.innerHTML = `${temperature} @ ${place}`;
+  });
+}
+
+// coordsObj 저장
 function saveCoords(coordsObj){
   localStorage.setItem(COORDS, JSON.stringify(coordsObj));
 }
 
+//위도 경도 위치를 받을 함수.
 function handleGeoSucces(position){
   const latitude = position.coords.latitude;
   const longitude = position.coords.longitude;
@@ -15,26 +31,31 @@ function handleGeoSucces(position){
     longitude
   };
   saveCoords(coordsObj)
+  getWeather(latitude, longitude);
 }
 
+// 위치정보 에러
 function handleGeoError() {
   console.log('Cant access geo location');
 }
 
+// 위치정보 권한 묻고 받는 것.
 function askForCoords() {
   navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
 }
 
+// COORDS를 저장 / 날씨 데이터 함수호출 
 function loadCoords() {
-  const loadedCords = localStorage.getItem(COORDS);
-  if(loadedCords === null){
+  const loadedCoords = localStorage.getItem(COORDS);
+  if(loadedCoords === null){
     askForCoords();
   } else {
-
+    const parsedCoords = JSON.parse(loadedCoords); // JS형식으로 loadedCords를 구문분석
+    getWeather(parsedCoords. latitude, parsedCoords.longitude); // 파싱된 위도와 경도를 가져온다.
   }
 }
 
-
+// 실행함수
 function init() {
   loadCoords();
 }
